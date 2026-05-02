8482a1fa7eb7dc7b3782d027670bda218b86322b
leo <leo@leos-MacBook-Air.local>
2026-05-02 19:23:33 +0800

    updated: 2026-05-02 19:23:33

--- code_leo/config/training.py
+++ code_leo/config/training.py
@@ -7,9 +7,9 @@ REGRESSION_TRAINING = {
     'train_batch_size': 512,
     'test_batch_size': 512,
     'lr': 0.0005,
-    'log_interval': 20,
+    'log_interval': 10,
     'num_epochs': 1000,
-    'val_interval': 5,
+    'val_interval': 1,
     'early_stop_patience': 50,
 }
 
